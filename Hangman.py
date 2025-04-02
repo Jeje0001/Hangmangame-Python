c4a31ca9e986f9ce9da1e7e7d62af99080a0dd52
jejea <157323284+Jeje0001@users.noreply.github.com>
2025-04-02 17:48:46 -0400

    done

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,6 @@
 import random
+from worldlist import words
 
-words=("apple","orange","banana","coconut","pineapple")
 
 hangman_art={0:("   ",
                 "   ",
@@ -28,13 +28,20 @@ hangman_art={0:("   ",
 # print(hangman_art[0])
 
 def display_man(wrong_guesses):
-    pass
+    print("************")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("************")
+
+
 
 def display_hint(hint):
-    pass
+
+    print(" ".join(hint))
 
 def display_answer(answer):
-    pass
+    print(" ".join(answer))
+
 
 def main():
     
@@ -45,8 +52,36 @@ def main():
     is_running=True
 
     while is_running == True:
-        pass
-# continue at 6:17:19
-    print(hint)
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess=input("Enter a Letter ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid Input")
+            continue    
+        if guess in guessed_letters:
+            print(f"{guess} was already guessed")
+        guessed_letters.add(guess)
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i]  == guess:
+                    hint[i]=guess
+        else:
+           wrong_guesses+=1 
+        
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("You win")
+            is_running=False
+        elif wrong_guesses >= len(hangman_art) - 1:
+             display_man(wrong_guesses)
+             display_answer(answer)
+             print("You lose")
+             is_running=False
+
+
+
+
 if __name__ == "__main__":
     main()
---
+++ worldlist.py
@@ -0,0 +1,15 @@
+# Words for hangman Game
+
+
+words = (
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon",
+    "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli", "vanilla",
+    "watermelon", "xigua", "yam", "zucchini", "avocado", "blackberry", "blueberry", "cantaloupe", "dragonfruit", "eggplant",
+    "grapefruit", "huckleberry", "jackfruit", "kumquat", "lime", "mulberry", "olive", "peach", "plum", "pomegranate",
+    "apricot", "bilberry", "boysenberry", "coconut", "cranberry", "damson", "gooseberry", "jujube", "lychee", "medlar",
+    "persimmon", "plantain", "prune", "redcurrant", "salak", "satsuma", "tamarillo", "yuzu", "ackee", "breadfruit",
+    "carambola", "cherimoya", "feijoa", "guava", "ilang-ilang", "jabuticaba", "longan", "mangosteen", "nance", "pomelo",
+    "soursop", "surinam cherry", "tamarind", "wampi", "white currant", "miracle fruit", "monstera", "santol", "sapodilla", "star apple",
+    "sugar apple", "tangelo", "tayberry", "thorn apple", "white sapote", "wild plum", "chayote", "clementine", "finger lime", "jostaberry",
+    "kaffir lime", "mamoncillo", "mamey", "maracuja", "sapote", "ugni", "jaboticaba", "chempedak", "horned melon", "pequi"
+)
